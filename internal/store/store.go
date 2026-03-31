0c5233b93233761a6d3da794b2f451cd053282ef
Paryiartvy <maxdok87@gmail.com>
2026-04-01 02:38:43 +0300

    начало работы: реализована часть tore

--- internal/store/store.go
+++ internal/store/store.go
@@ -4,63 +4,148 @@
 package store
 
 import (
+	"fmt"
 	"gopherledger/internal/domain"
+	"slices"
+	"sync"
+	"time"
 )
 
 // Store хранит все данные приложения в памяти.
 // Добавьте средства защиты конкурентного доступа самостоятельно.
 type Store struct {
+	userMu sync.RWMutex
 	// users хранит пользователей по их ID
 	users map[int64]*domain.User
 
 	// usersByLogin хранит пользователей по логину - для быстрого поиска при авторизации
 	usersByLogin map[string]*domain.User
 
+	// nextID используется для генерации уникальных числовых ID
+	nextID int64
+
+	ordersMu sync.RWMutex
 	// orders хранит заказы по номеру заказа
 	orders map[string]*domain.Order
 
+	balanceMu sync.RWMutex
 	// balances хранит текущий баланс каждого пользователя по его ID
 	balances map[int64]*domain.Balance
 
 	// withdrawals хранит историю списаний для каждого пользователя по его ID
 	withdrawals map[int64][]*domain.Withdrawal
-
-	// nextID используется для генерации уникальных числовых ID
-	nextID int64
 }
 
 // New создаёт и возвращает новое пустое хранилище.
 func New() *Store {
-	panic("не реализовано")
+	return &Store{
+		users:        make(map[int64]*domain.User),
+		usersByLogin: make(map[string]*domain.User),
+		nextID:       1,
+		orders:       make(map[string]*domain.Order),
+		balances:     make(map[int64]*domain.Balance),
+		withdrawals:  make(map[int64][]*domain.Withdrawal),
+	}
 }
 
 // CreateUser добавляет нового пользователя.
 // Возвращает domain.ErrUserExists если логин уже занят.
 func (s *Store) CreateUser(login, passwordHash string) (*domain.User, error) {
-	panic("не реализовано")
+	s.userMu.Lock()
+	defer s.userMu.Unlock()
+	if _, ok := s.usersByLogin[login]; ok {
+		return &domain.User{}, fmt.Errorf("registration error: %w", domain.ErrUserExists)
+	}
+	user := domain.User{
+		ID:           s.nextID,
+		Login:        login,
+		PasswordHash: passwordHash,
+	}
+
+	s.usersByLogin[login] = &user
+	s.users[s.nextID] = &user
+	s.nextID += 1
+
+	return &user, nil
 }
 
 // GetUserByLogin возвращает пользователя по логину.
 // Возвращает domain.ErrUserNotFound если пользователь не найден.
 func (s *Store) GetUserByLogin(login string) (*domain.User, error) {
-	panic("не реализовано")
+	s.userMu.RLock()
+	defer s.userMu.RUnlock()
+
+	if user, ok := s.usersByLogin[login]; ok {
+		return user, nil
+	}
+	return &domain.User{}, fmt.Errorf("user find error: %w", domain.ErrUserNotFound)
 }
 
 // CreateOrder добавляет новый заказ для пользователя.
 // Возвращает domain.ErrOrderOwnedByUser если этот пользователь уже загружал этот номер.
 // Возвращает domain.ErrOrderExists если номер принадлежит другому пользователю.
 func (s *Store) CreateOrder(userID int64, number string) (*domain.Order, error) {
-	panic("не реализовано")
+	s.ordersMu.Lock()
+	defer s.ordersMu.Unlock()
+
+	if order, ok := s.orders[number]; ok {
+		if order.UserID == userID {
+			return &domain.Order{}, fmt.Errorf("order create error: %w", domain.ErrOrderOwnedByUser)
+		} else {
+			return &domain.Order{}, fmt.Errorf("order create error: %w", domain.ErrOrderExists)
+		}
+	}
+
+	s.userMu.Lock()
+	defer s.userMu.Unlock()
+
+	order := domain.Order{
+		ID:         s.nextID,
+		UserID:     userID,
+		Number:     number,
+		Status:     domain.OrderStatusNew,
+		Accrual:    0,
+		UploadedAt: time.Now(),
+	}
+	s.orders[number] = &order
+	s.nextID += 1
+	return &order, nil
 }
 
 // GetUserOrders возвращает все заказы пользователя, сначала новые.
 func (s *Store) GetUserOrders(userID int64) ([]domain.Order, error) {
-	panic("не реализовано")
+	orders := make([]domain.Order, 0)
+
+	s.ordersMu.RLock()
+	defer s.ordersMu.RUnlock()
+
+	for _, order := range s.orders {
+		if order.UserID == userID {
+			orders = append(orders, *order)
+		}
+	}
+	slices.SortFunc(orders, func(a, b domain.Order) int {
+		return b.UploadedAt.Compare(a.UploadedAt)
+	})
+	return orders, nil
 }
 
 // GetOrdersForProcessing возвращает все заказы в статусе NEW или PROCESSING.
 func (s *Store) GetOrdersForProcessing() ([]domain.Order, error) {
-	panic("не реализовано")
+	orders := make([]domain.Order, 0)
+
+	s.ordersMu.RLock()
+	defer s.ordersMu.RUnlock()
+
+	for _, order := range s.orders {
+		if order.Status == domain.OrderStatusNew || order.Status == domain.OrderStatusProcessing {
+			orders = append(orders, *order)
+		}
+	}
+	slices.SortFunc(orders, func(a, b domain.Order) int {
+		return b.UploadedAt.Compare(a.UploadedAt)
+	})
+	return orders, nil
 }
 
 // UpdateOrderStatus обновляет статус и начисление заказа.
